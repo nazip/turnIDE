d68f14b6b0bf172a3f0c6ac75714443c32952f7b
nazip <nazipm@mail.ru>
2017-05-24 11:25:10 +0300

    add flightplan

---
+++ flightplan.js
@@ -0,0 +1,37 @@
+var plan = require('flightplan');
+
+var tempDir = 'blog-client' + new Date().getTime();
+var user = 'react';
+
+plan.taget('production', {
+  host: '127.0.0.1',
+  username: user,
+  agent: process.env.SSH_AUTH_SOCK
+});
+
+plan.local(function(local) {
+  local.log('copy files to remote host');
+  var filesToCopy = local.exec('git-ls-files', {silent: true});
+  local.transfer(filesToCopy, '/tmp/', tempDir);
+});
+
+plan.remote(function(remote) {
+  remote.exec('nvm use default');
+  remote.log('Move folder to web root');
+  remote.exec('cp -R /tmp/' + tempDir + '~');
+  remote.rm('-rf /tmp/' + tempDir);
+
+  remote.log('install dependencies');
+  remote.exec('npm --prefix ~/' + tempDir + ' install ~ /' + tempDir);
+
+  remote.log('Buid');
+  remote.exec('cd ~/' + tempDir + '/semantic && gulp build');
+  remote.exec('npm --prefix ~/' + tempDir + ' run build');
+
+  remote.log('Reload application');
+  remote.exec('ln -snf ~/' + tempDir + ' ~/current');
+  remote.exec('(cd ~/current && pm2 restart pm2.config.js --env production)');
+
+  remote.log('Finish');
+  
+});
